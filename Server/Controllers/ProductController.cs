a25ce0a2da4e1dcdaa4b6c0631d745c9b52d6e1e
Si Lam <silam@hotmail.com>
2024-08-11 21:02:13 -0500

    Product service

--- Server/Controllers/ProductController.cs
+++ Server/Controllers/ProductController.cs
@@ -6,9 +6,42 @@ namespace UNet7BlazorAppEcomm.Server.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        
+
+        private static List<Product> Products = new List<Product>
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            new Product
+            {
+                Id = 1,
+                Title="Mastering Blockchain - Third Edition",
+                Description="A deep dive into distributed ledgers, consensus protocols, smart contracts, DApps, cryptocurrencies, Ethereum, and more 3rd ed. Edition",
+                Price = 9.99m,
+                ImageUrl = "https://m.media-amazon.com/images/I/71sZn444OfL._SL1360_.jpg"
+            },
+            new Product
+            {
+                Id = 2,
+                Title="Azure SQL Revealed",
+                Description="A Guide to the Cloud for SQL Server Professionals 1st ed. Edition, Kindle Edition",
+                Price = 90.99m,
+                ImageUrl = "https://m.media-amazon.com/images/I/61kXeAWGSbL._SL1180_.jpg"
+            },
+            new Product
+            {
+                Id = 3,
+                Title="Algorithms and Data Structures with Python",
+                Description="An interactive learning experience: Comprehensive introduction to data structures and algorithms (Spanish Edition) Kindle Edition",
+                Price = 19.99m,
+                ImageUrl = "https://m.media-amazon.com/images/I/81Xp21sgKIL._SL1500_.jpg"
+            },
+            new Product
+            {
+                Id = 4,
+                Title="System Design Interview",
+                Description="System design questions are often the most difficult of all technical interview questions. This book makes them easier to tackle. It is Volume 1 of the ‘System Design Interview - An Insider’s Guide’ series",
+                ImageUrl = "https://m.media-amazon.com/images/I/615KGTRs1OL._SL1500_.jpg",
+                Price = 39.99m
+            }
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -19,15 +52,9 @@ namespace UNet7BlazorAppEcomm.Server.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok(Products);
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -8,8 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
+
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -22,6 +28,9 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwagger();
+
 
 app.UseHttpsRedirection();
 
